45b969d94953101251a8b413e292b40175c063f0
Mucahit Tutuncu <tutuncumucahit@gmail.com>
2015-06-21 04:20:18 +0300

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglifyjs');
+
+var config = {
+    bootstrapDir: 'bower_components/bootstrap-sass',
+    bowerDir: 'bower_components',
+    publicDir: 'public',
+    scssDirectory: 'sources/scss/**/*.scss'
+};
+
+gulp.task('styles', function() {
+    gulp.src(config.scssDirectory)
+        .pipe(sass({
+            errLogToConsole: true,
+            style: 'compressed',
+            includePaths: [config.bootstrapDir + '/assets/stylesheets']
+        }))
+        .pipe(gulp.dest(config.publicDir+'/assets/css/'));
+});
+
+gulp.task('js', function() {
+  gulp.src([
+    config.bowerDir + '/jquery/dist/jquery.min.js',
+    config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.js',
+  ])
+  .pipe(uglify('app.js', {
+    compress: true,
+    outSourceMap: true,
+  }))
+  .pipe(gulp.dest(config.publicDir + '/js'));
+});
+
+gulp.task('fonts', function() {
+    gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+    .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(config.scssDirectory,['styles']);
+    gulp.watch('sources/js/**/*.js',['js']);
+});
+
+gulp.task('default', ['styles', 'js', 'fonts']);
